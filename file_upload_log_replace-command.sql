4c8047ae1db2a39a285e14a6091ea79d6865127a
ullas.g <ullas.g@techjini.com>
2019-10-24 11:24:33 +0530

    deleted files

--- file_upload_log_replace-command.sql
+++
@@ -1,2 +0,0 @@
-UPDATE file_upload_details SET property = REPLACE(property, '{"propertyName":"isOSVEnabled","propertyValue":false},', '')
-
--- sessions.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `sessions` (
-  `sess_id` varchar(128) COLLATE utf8_bin NOT NULL,
-  `sess_data` blob NOT NULL,
-  `sess_time` int(10) UNSIGNED NOT NULL,
-  `sess_lifetime` mediumint(9) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
